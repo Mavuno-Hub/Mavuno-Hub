6c875b1b6decb11714945372e7cea29945f9648a
Collins Koech <42299025+collinsvictor1818@users.noreply.github.com>
2023-09-25 09:30:17 +0300

    First Commit
    A Skeleton Code Template for a responsive website/mobile app

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromARGB(255, 255, 174, 0);
+const secondaryColor = Color(0xFF373737);
+const bgColor = Color(0xFF222222);
+
+const defaultPadding = 16.0;
